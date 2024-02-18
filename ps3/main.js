d899ffe12ad7710b1fcd0dde0e7ec880955d221c
swetha.sriram <sswetha@umich.edu>
2024-02-17 20:18:19 -0500

    last

--- ps3/main.js
+++ ps3/main.js
@@ -1,5 +1,5 @@
-const UMevents = [];
-const timeout_id = null; 
+let UMevents = [];
+let timeout_id = null; 
 //want the timeout id to be empty so we set to null
 
 getUMEventsWithImages((eventsData) =>{
@@ -11,7 +11,7 @@ getUMEventsWithImages((eventsData) =>{
         const imgElem = document.createElement("img");
         //creating an image that will be in the UM list and in the thumbnail div 
         imgElem.setAttribute('id',`thumb-${currentEv.id}`);
-        //set id to current id number
+        //set id to current id number 
         //this is only in the list UM events 
         imgElem.setAttribute('src', currentEv.styled_images.event_thumb);
         //current image that we are on were getting the URL of the image through source
@@ -19,43 +19,47 @@ getUMEventsWithImages((eventsData) =>{
         //styled_images.event_thumb is the url source 
         thumbnails.append(imgElem);
         //were appending the image element to the thumbnail div
-
+        imgElem.addEventListener("click", () =>{
+            setSelectedIndex (i)
+        }); 
+        //adding event listener to every thumbnail
     }
-        
+    setSelectedIndex(0);
+    //when you initialize the webpage it tells it to start at the first thumbnail
 });
 
-function setSelectedIndex (selectedEvent){
-    const evTitle = selectedEvent.event_title;
-    const evLink= selectedEvent.permalink;
-    const evImg = selectedEvent.image_url;
-    const evStartTime = getReadableTime(selectedEvent.datetime_start);
-    const evDesc = selectedEvent.description
+function setSelectedIndex (i) {
+   
+    const selectedEvent = UMevents[i];
+     //set time out has two arguments: function refrence and the time 
+    //function we want to run after 10 sections is set selected index of the next item/img
+    //modular function, returns remainder of division function. dividing length by #of events and if it is remainder 1 returns to index 0 
 
-    document.querySelector('#selected-title').innertext = evTitle;
+    
+
+    document.querySelector('#selected-title').innerText = selectedEvent.event_title;
     //sets inner text to title
-    document.querySelector('#selected-title').setAttribute('href', evLink);
+    document.querySelector('#selected-title').setAttribute('href', selectedEvent.permalink);
     //sets title href to img src url 
-    document.querySelector('#selected-date').innertext = evStartTime;
+    document.querySelector('#selected-date').innerText =  getReadableTime(selectedEvent.datetime_start);
 
-    document.querySelector('selected-image').setAttribute('src', evImg);
+    document.querySelector('selected-image').setAttribute('src', selectedEvent.image_url);
 
-    document.querySelector('#selected-description').innertext = evDesc;
+    document.querySelector('#selected-description').innerText = selectedEvent.description;
+
+    let selected_list = document.querySelectorAll('.selected')
+    for (const select of selected_list){
+        select.classList.remove('selected');
+    }
+
+    document.querySelector(`thumb-${selectedEvent.id}`).classList.add('selected');
 
     if (timeout_id){
         clearTimeout(timeout_id);
     }
     //clears timeout
     timeout_id = setTimeout(()=> {
-        setSelectedIndex((selectedEvent+1)%events.length);
+        setSelectedIndex((selectedEvent+1)%UMevents.length);
     }, 10000)
-    //set time out has two arguments: function refrence and the time 
-    //function we want to run after 10 sections is set selected index of the next item/img
-    //modular function, returns remainder of division function. dividing length by #of events and if it is remainder 1 returns to index 0 
-
+    
 }
-
-
-imgElem.addEventListener("onclick", (ev) =>{
-    imgElem.classList.add("selected");
-    setSelectedIndex (imgElem.id);
-});
